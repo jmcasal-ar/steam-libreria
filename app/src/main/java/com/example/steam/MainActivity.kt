cc7ede08f2499035d033cd2c5f0715126c705b00
Juan <casal.juanmanuel@gmail.com>
2020-09-30 18:40:20 -0300

    Agregando videojuegos

--- app/src/main/java/com/example/steam/MainActivity.kt
+++ app/src/main/java/com/example/steam/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.steam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  rvGames : RecyclerView
+    private val adapter: GamesAdapter by lazy { GamesAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupUI()
+        retrieveGames()
+    }
+
+    private fun retrieveGames() {
+        val games = GamesProvider.getGames()
+        adapter.updateGames(games)
+    }
+
+    private fun setupUI() {
+        rvGames = findViewById(R.id.rvGames)
+        rvGames.adapter = adapter
     }
 }
